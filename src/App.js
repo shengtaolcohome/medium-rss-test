6b6ab47acde9911391a2b7a4198fb1fd4f9b988b
Shengtao Li <shengtaol@cohomenow.com>
2020-01-30 20:51:27 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    html: null,
+  }
+
+  componentDidMount() {
+    return axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@KonradDaWo')
+      .then(res => {
+        console.log("res is >>>", res);
+        const item = res.data['items'][0];
+        this.setState({ html: item.content });
+      });
+  }
+
+  render() {
+    console.log("this.state.html is >>>", this.state.html);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12" id="test" dangerouslySetInnerHTML={{__html: this.state.html}} />
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
